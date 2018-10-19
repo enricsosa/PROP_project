dd05af1522a699657a4e9f4d8157b7c58491c9b6
enricsosa <enricsosa@gmail.com>
2018-10-19 21:14:50 +0200

    Capa de dades

--- FONTS/Main.java
+++ FONTS/Main.java
@@ -1,6 +1,20 @@
+import data.CtrlAsignaturasFile;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.LinkedList;
+import java.util.List;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+import java.io.FileNotFoundException;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException, IOException, ParseException {
+
         System.out.println("Hello World!");
+
+        List<JSONObject> asignaturasdata = CtrlAsignaturasFile.getInstance().getAll();
     }
 }
--- FONTS/data/CtrlAsignaturasFile.java
+++ FONTS/data/CtrlAsignaturasFile.java
@@ -1,16 +1,56 @@
 package data;
 
 /** Imports **/
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.LinkedList;
+import java.util.List;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+import java.io.FileNotFoundException;
+
 
 public class CtrlAsignaturasFile {
 
     /** Atributos **/
+    private static CtrlAsignaturasFile infoAsignaturas;
 
     /** Constructoras **/
+
+    private CtrlAsignaturasFile() {
+    }
+
+    public static CtrlAsignaturasFile getInstance() {
+        if (infoAsignaturas == null)
+            infoAsignaturas = new CtrlAsignaturasFile() {
+            };
+        return infoAsignaturas;
+    }
 
     /** Métodos públicos **/
 
     /** Consultoras **/
+
+    public List<JSONObject> getAll() throws FileNotFoundException, IOException, ParseException {
+        LinkedList<JSONObject> asignaturas = new LinkedList<JSONObject>();
+
+        JSONObject obj = (JSONObject)new JSONParser().parse(new FileReader("DATA/asignaturas.json"));
+        JSONArray a = (JSONArray)obj.get("asignaturas");
+
+        for (Object o : a) {
+            JSONObject assignatura = (JSONObject) o;
+
+            String id = (String) assignatura.get("id");
+            System.out.println(id);
+
+            asignaturas.add(assignatura);
+        }
+
+        return asignaturas;
+    }
 
     /** Métodos redefinidos **/
 
