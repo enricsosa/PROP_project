7ce93922107ba7aca779fd03bdcbd5f08e867c03
enricsosa <enricsosa@gmail.com>
2018-10-20 19:50:32 +0200

    Initialized CtrlDomain

---
+++ FONTS/domaincontrollers/CtrlDomain.java
@@ -0,0 +1,54 @@
+package domaincontrollers;
+
+import java.io.IOException;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.HashMap;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+import java.io.FileNotFoundException;
+
+import data.CtrlAsignaturasFile;
+import data.CtrlAulasFile;
+import domain.Asignatura;
+import domain.Aula;
+
+public class CtrlDomain {
+
+    /** Atributos **/
+    private CtrlAsignaturasFile controladorAsignaturas;
+    private CtrlAulasFile controladorAulas;
+    private Map<String, Asignatura> asignaturas;
+    private Map<String, Aula> aulas;
+
+    /** Constructoras **/
+
+    public CtrlDomain() {
+        controladorAsignaturas = CtrlAsignaturasFile.getInstance();
+        controladorAulas = CtrlAulasFile.getInstance();
+        asignaturas = new HashMap<String, Asignatura>();
+        aulas = new HashMap<String, Aula>();
+    }
+
+    /** Métodos públicos **/
+
+    public Map<String, Asignatura> cargarAllAsignaturas() throws FileNotFoundException, IOException, ParseException {
+        List<JSONObject> asignaturasData = controladorAsignaturas.getAll();
+        Map<String, Asignatura> list = new HashMap<String, Asignatura>();
+
+        for (JSONObject o : asignaturasData) {
+            Asignatura asignatura = new Asignatura((String)o.get("id"), (String)o.get("nombre"));
+            this.asignaturas.put(asignatura.getId(), asignatura);
+            list.put(asignatura.getId(), asignatura);
+        }
+
+        return list;
+    }
+
+    /** Consultoras **/
+
+    /** Métodos redefinidos **/
+}
