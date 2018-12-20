8c6ae8693efa6fc5c6a35b01cf4394fed80ed3ad
enricsosa <enricsosa@gmail.com>
2018-12-21 00:38:14 +0100

    GUI editAsigs en proces

--- FONTS/presentation/FXMLControllers/CtrlEditGrupos.java
+++ FONTS/presentation/FXMLControllers/CtrlEditGrupos.java
@@ -47,11 +47,13 @@ public class CtrlEditGrupos {
     /**Restriciones despues de editar.*/
     private HashMap<String, ArrayList<Object>> restriccionesFinal;
     /**Restriccion seleccionada.*/
-    private Integer currentSes;
+    private char currentGr;
     /**Restriccion seleccionada.*/
-    private ArrayList<Object> currentSesAttr;
+    private char currentSGr;
+    private String currentType;
+    private Integer currentPlazas;
     /**idAsignatura1.*/
-    private ArrayList<ArrayList<Object>> currentSesiones;
+    private ArrayList<ArrayList<Object>> currentGrupos;
     /**idAsignatura2.*/
     private String currentAs2 = null;
     /**Instancia de CtrlDomain.*/
@@ -114,7 +116,7 @@ public class CtrlEditGrupos {
     }
 
 
-    //EDIT LAYOUT
+    //REMOVE LAYOUT
     @FXML
     Label idGrm;
     @FXML
@@ -137,6 +139,36 @@ public class CtrlEditGrupos {
 
     private void setLayout() {
 
+        //EDIT & REMOVE
+        rbTrm.setDisable(true);
+        rbPrm.setDisable(true);
+        rbLrm.setDisable(true);
+        if (currentType.equals("Teoria")) {
+            rbTedit.setSelected(true);
+            rbPedit.setSelected(false);
+            rbLedit.setSelected(false);
+            rbTrm.setSelected(true);
+            rbPrm.setSelected(false);
+            rbLrm.setSelected(false);
+        } else if (currentType.equals("Problemas")) {
+            rbTedit.setSelected(false);
+            rbPedit.setSelected(true);
+            rbLedit.setSelected(false);
+            rbTrm.setSelected(false);
+            rbPrm.setSelected(true);
+            rbLrm.setSelected(false);
+        } else if (currentType.equals("Laboratorio")) {
+            rbTedit.setSelected(false);
+            rbPedit.setSelected(false);
+            rbLedit.setSelected(true);
+            rbTrm.setSelected(false);
+            rbPrm.setSelected(false);
+            rbLrm.setSelected(true);
+        }
+        idGrm.setText(Character.toString(currentGr));
+        idSrm.setText(Character.toString(currentSGr));
+        plazasIdrm.setText(currentPlazas.toString());
+        plazasIdedit.setText(currentPlazas.toString());
     }
 
     @FXML
@@ -148,21 +180,97 @@ public class CtrlEditGrupos {
     private void itemSelected(ObservableValue<? extends String> observableValue, String s, String t1) {
         if (!(t1 == null)) {
 
-            //listview.getSelectionModel().selectedItemProperty().removeListener(grSelected);
-            //listview.getSelectionModel().selectedItemProperty().addListener(grSelected);
+            listview.getSelectionModel().selectedItemProperty().removeListener(grSelected);
+            listview.getSelectionModel().selectedItemProperty().addListener(grSelected);
 
-            //currentSes = Integer.parseInt(t1.substring(t1.indexOf("#")+1, t1.indexOf(" ")));
+            currentGr = t1.charAt(0);
+            currentSGr = t1.charAt(1);
+            currentType = t1.substring(t1.indexOf(" ")+1);
+            for (ArrayList<Object> g: currentGrupos) {
+                String idG = (String)g.get(0);
+                if (idG.equals(Character.toString(currentGr))) {
+                    ArrayList<ArrayList<Object>> subgr = (ArrayList<ArrayList<Object>>)g.get(1);
+                    for (ArrayList<Object> sg : subgr) {
+                        if (sg.get(0).equals(Character.toString(currentSGr))) {
+                            currentPlazas = (Integer) sg.get(1);
+                        }
+                    }
+                }
+            }
+            System.out.println(currentGr);
+            System.out.println(currentSGr);
+            System.out.println(currentType);
+            System.out.println(currentPlazas);
             setLayout();
         }
     }
 
     private void setRadioButtons(String rb) {
-
+        switch (rb) {
+            case "Tadd":
+                rbPadd.setSelected(false);
+                rbLadd.setSelected(false);
+                break;
+            case "Padd":
+                rbTadd.setSelected(false);
+                rbLadd.setSelected(false);
+                break;
+            case "Ladd":
+                rbTadd.setSelected(false);
+                rbPadd.setSelected(false);
+                break;
+            case "Tedit":
+                rbPedit.setSelected(false);
+                rbLedit.setSelected(false);
+                break;
+            case "Pedit":
+                rbTedit.setSelected(false);
+                rbLedit.setSelected(false);
+                break;
+            case "Ledit":
+                rbTedit.setSelected(false);
+                rbPedit.setSelected(false);
+                break;
+            default:
+                break;
+        }
     }
 
     /**Asigna un elemento a layout.*/
     public void display(boolean editing, ArrayList<ArrayList<Object>> grupos) {
+        if (editing) {
+            currentGrupos = grupos;
+            System.out.println(grupos);
 
+            listview.getItems().clear();
+            Integer i = 1;
+            for (ArrayList<Object> g: grupos) {
+                String idG = (String)g.get(0);
+                ArrayList<ArrayList<Object>> subgr = (ArrayList<ArrayList<Object>>)g.get(1);
+                for (ArrayList<Object> sg : subgr) {
+                    listview.getItems().add(idG + sg.get(0) + " " + sg.get(2));
+                }
+            }
+            listview.getSelectionModel().selectedItemProperty().removeListener(grSelected);
+            listview.getSelectionModel().selectedItemProperty().addListener(grSelected);
+        } else {
+            currentGrupos = new ArrayList<>();
+        }
+        idGadd.getItems().clear();
+        idGadd.setItems(FXCollections.observableArrayList("1", "2", "3", "4", "5", "6", "7", "8", "9"));
+        idSadd.getItems().clear();
+        idSadd.setItems(FXCollections.observableArrayList("0", "1", "2", "3", "4", "5", "6", "7", "8", "9"));
+        idGedit.getItems().clear();
+        idGedit.setItems(FXCollections.observableArrayList("1", "2", "3", "4", "5", "6", "7", "8", "9"));
+        idSedit.getItems().clear();
+        idSedit.setItems(FXCollections.observableArrayList("0", "1", "2", "3", "4", "5", "6", "7", "8", "9"));
+
+        rbTadd.setOnAction( e -> setRadioButtons("Tadd"));
+        rbPadd.setOnAction( e -> setRadioButtons("Padd"));
+        rbLadd.setOnAction( e -> setRadioButtons("Ladd"));
+        rbTedit.setOnAction( e -> setRadioButtons("Tedit"));
+        rbPedit.setOnAction( e -> setRadioButtons("Pedit"));
+        rbLedit.setOnAction( e -> setRadioButtons("Ledit"));
     }
 
 }
